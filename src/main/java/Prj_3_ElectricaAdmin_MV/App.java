11fe8c2bcff87b191b87bc0756543962d57456fb
Noane Dan <Noane.Dan@outlook.com>
2018-03-20 18:45:25 +0200

    Project skeleton

---
+++ src/main/java/Prj_3_ElectricaAdmin_MV/App.java
@@ -0,0 +1,13 @@
+package Prj_3_ElectricaAdmin_MV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
