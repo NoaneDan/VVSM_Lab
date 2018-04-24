0eb9bb074f0a4cd661ece45163f89a0e6dfbbbf6
Noane Dan <Noane.Dan@outlook.com>
2018-04-24 18:34:15 +0300

    Magic Test

--- src/test/java/Prj_3_ElectricaAdmin_MV/AppTest.java
+++ src/test/java/Prj_3_ElectricaAdmin_MV/AppTest.java
@@ -1,8 +1,15 @@
 package Prj_3_ElectricaAdmin_MV;
 
+import Prj_3_ElectricaAdmin_MV.controller.ClientController;
+import Prj_3_ElectricaAdmin_MV.controller.ClientControllerTest;
+import Prj_3_ElectricaAdmin_MV.model.Client;
+import Prj_3_ElectricaAdmin_MV.model.Issue;
+import Prj_3_ElectricaAdmin_MV.repository.DataManager;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import java.io.File;
 
 /**
  * Unit test for simple App.
@@ -10,6 +17,9 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
+    private ClientController controller;
+    private DataManager dataManager;
+
     /**
      * Create the test case
      *
@@ -34,5 +44,79 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+
+        dataManager = new DataManager("testClient.txt", "testIssue.txt");
+        controller = new ClientController(dataManager);
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+
+        File clientFile = new File("testClient.txt");
+        File issueFile = new File("testIssue.txt");
+
+        if (clientFile.exists()) {
+            clientFile.delete();
+        }
+        if (issueFile.exists()) {
+            issueFile.delete();
+        }
+    }
+
+    public void testA() {
+
+        String result = controller.AddClient("ion", "8, Alee, Cluj", "0");
+
+        assertNull(result);
+        assertEquals(dataManager.Clients.size(), 1);
+
+        Client client = dataManager.Clients.get(dataManager.Clients.size() - 1);
+        assertEquals(client.idClient, "0");
+        assertEquals(client.Name, "ion");
+        assertEquals(client.Address, "8, Alee, Cluj");
+    }
+
+    public void testB() {
+
+        controller.AddClient("ion", "8, Alee, Cluj", "0");
+        Client c = new Client("ion", "8, Alee, Cluj", "0");
+
+        String result1 = controller.AddClientIndex(c, 2018, 5, 200);
+        String result2 = controller.AddClientIndex(c, 2018, 4, 200);
+
+        assertNull(result1);
+        assertNull(result2);
+        assertEquals(dataManager.Issues.size(), 2);
+
+        Issue i = dataManager.Issues.get(dataManager.Issues.size() - 1);
+        assertEquals(i.Year, 2018);
+        assertEquals(i.Month, 4);
+        assertEquals((double) i.ToPay, (double) 200);
+        assertEquals(i.Client, c);
+    }
+
+    public void testC() {
+
+        Client c = new Client("ion", "8, Alee, Cluj", "0");
+
+        controller.AddClient("ion", "8, Alee, Cluj", "0");
+        controller.AddClientIndex(c, 2018, 5, 200);
+        controller.AddClientIndex(c, 2018, 4, 200);
+
+        String result = controller.ListIssue(c);
+
+        assertNotNull(result);
+    }
+
+    public void bigBangTesting() {
+
+        testA();
+        testB();
+        testC();
     }
 }
